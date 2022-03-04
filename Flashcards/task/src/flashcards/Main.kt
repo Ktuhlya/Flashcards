88337a829109864956c2947ceb0899eec498dd71
duddon <kktuhlya@gmail.com>
2022-03-04 16:00:31 +0700

    212

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,10 +1,12 @@
 package flashcards
 
+val cardList = mutableListOf<Card>()
+
 fun main() {
   print("Input the number of cards:\n> ")
-  val cardList = MutableList<Card>(readln().toInt()) { Card() }
-  for (i in 1..cardList.size) {
-    cardList[i-1] = Card().create(i)
+
+  for (i in 1..readln().toInt()) {
+    cardList.add(i-1, Card().create(i))
   }
     for (i in 1..cardList.size)
    tiDebil(i-1, cardList)
@@ -14,15 +16,44 @@ fun tiDebil(i: Int, cardList: MutableList<Card>) {
     println("Print the definition of \"${cardList[i].term}\":")
     val str = readln()
     if (str == cardList[i].definition) println("Correct!")
-              else println("Wrong. The right answer is \"${cardList[i].definition}\".")
+    else {
+        if (checkRepeat(str)) {
+            println(
+                "Wrong. The right answer is \"${cardList[i].definition}\", " +
+                        "but your answer is " +
+                        "correct for \"${checkRepeatAnswer(str)}\"."
+            )
+        } else {
+            println("Wrong. The right answer is \"${cardList[i].definition}\".")
+        }
+    }
 }
 
-class Card(var term: String = "", var definition: String= ""){
-  fun create(i: Int): Card {
-      print("Card #$i:\n> ")
-      term = readln()
-      print("The definition for card #$i:\n> ")
-       definition = readln()
-    return Card(term, definition)
-  }
+fun checkRepeatAnswer(str: String): String{
+    cardList.forEach { if(it.definition == str) return it.term }
+    return ""
 }
+
+fun  checkRepeat ( str: String): Boolean{
+    cardList.forEach{if(it.term==str || it.definition==str) return true }
+       return false
+}
+
+class Card(var term: String = "", var definition: String= "") {
+    fun create(i: Int): Card {
+        print("Card #$i:\n> ")
+          term = readln()
+          while (checkRepeat(term)) {
+              println("The term \"$term\" already exists. Try again:")
+              term = readln()
+          }
+        print("The definition for card #$i:\n> ")
+        definition = readln()
+        while (checkRepeat(definition)) {
+            println("The definition \"$definition\" already exists. Try again:")
+             definition = readln()
+            }
+        return Card(term, definition)
+    }
+}
+
